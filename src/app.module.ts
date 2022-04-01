7e162ba5276a60fcbe8aa0186705494908e3f397
Almas2002 <73034060+Almas2002@users.noreply.github.com>
2022-04-01 16:59:32 +0600

    third commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,18 @@ import { User } from './user/user.entity';
 import { Role } from './role/role.entity';
 import { Auth } from './entities/auth.entity';
 import { ConfigModule } from '@nestjs/config';
-
+require("dotenv").config()
 
 @Module({
   imports: [
     AuthModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '12345',
-      database: 'auth',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User, Role, Auth],
       synchronize: true,
     })
